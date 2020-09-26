031bf2bb6336a9ae9ea5ecc7316265192c9fc52a
Gennady <gennady.dev@gmail.com>
2020-09-27 00:27:33 +0400

    adding Markers

--- app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
 
@@ -52,6 +53,35 @@ class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
     else -> super.onOptionsItemSelected(item)
   }
 
+  private fun setMapLongClick(map: GoogleMap) {
+    map.setOnMapLongClickListener { latLng ->
+      // A Snippet is Additional text that's displayed below the title.
+      val snippet = String.format(
+        Locale.getDefault(),
+        "Lat: %1$.5f, Long: %2$.5f",
+        latLng.latitude,
+        latLng.longitude
+      )
+      map.addMarker(
+        MarkerOptions()
+          .position(latLng)
+          .title(getString(R.string.dropped_pin))
+          .snippet(snippet)
+      )
+    }
+  }
+
+  private fun setPoiClick(map: GoogleMap) {
+    map.setOnPoiClickListener { poi ->
+      val poiMarker = map.addMarker(
+        MarkerOptions()
+          .position(poi.latLng)
+          .title(poi.name)
+      )
+      poiMarker.showInfoWindow()
+    }
+  }
+
   /**
    * Manipulates the map once available.
    * This callback is triggered when the map is ready to be used.
@@ -64,9 +94,14 @@ class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
   override fun onMapReady(googleMap: GoogleMap) {
     map = googleMap
 
-    // Add a marker in Sydney and move the camera
-    val sydney = LatLng(-34.0, 151.0)
-    map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-    map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    val latitude = 53.239695
+    val longitude = 50.340672
+    val homeLatLng = LatLng(latitude, longitude)
+    val zoomLevel = 20f
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+    map.addMarker(MarkerOptions().position(homeLatLng))
+
+    setMapLongClick(map)
+    setPoiClick(map)
   }
 }
