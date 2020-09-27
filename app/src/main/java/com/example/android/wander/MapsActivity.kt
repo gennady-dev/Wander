6998a05991e110df6147890153910dbf95534a0b
Gennady <gennady.dev@gmail.com>
2020-09-28 01:54:45 +0400

    adding an overlay

--- app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -11,10 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.*
 
 class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
@@ -121,8 +118,13 @@ class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
     val latitude = 53.239695
     val longitude = 50.340672
     val homeLatLng = LatLng(latitude, longitude)
-    val zoomLevel = 15f
+    val zoomLevel = 17f
     map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+    val overlaySize = 100f
+    val androidOverlay = GroundOverlayOptions()
+      .image(BitmapDescriptorFactory.fromResource(R.drawable.android))
+      .position(homeLatLng, overlaySize)
+    map.addGroundOverlay(androidOverlay)
     map.addMarker(MarkerOptions().position(homeLatLng))
 
     setMapLongClick(map)
